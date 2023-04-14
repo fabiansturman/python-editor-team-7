fbdb366ee7db4920588fc823ef92f7d8a15c70df
hoaduong7 <hoaduong@protonmail.com>
2023-04-14 14:54:13 +0100

    temporary fix for editor not showing all content

--- src/documentation/tutorials/onChange.ts
+++ src/documentation/tutorials/onChange.ts
@@ -9,9 +9,17 @@ export default function onChange(editorState: EditorState): void {
     const json = editorState.toJSON();
     const str = JSON.stringify(json);
     const jsonState = JSON.parse(str);
+    console.log(str);
     if (typeof active !== "undefined") {
       active.stepTitle = jsonState.root.children[0].children[0].text;
-      active.content = jsonState.root.children[1].children[0].text;
+      var x = "";
+      for (let i = 1; i < jsonState.root.children.length; i++) {
+        if (typeof jsonState.root.children[i].children[0] !== "undefined") {
+          x = x.concat(" ", jsonState.root.children[i].children[0].text);
+        }
+      }
+      active.content = x;
+
     }
   });
 }
